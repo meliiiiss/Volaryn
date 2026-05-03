c80a48621e740209e76133e0e9591730981a76eb
meliiiiss <melissabatgut@gmail.com>
2026-05-03 13:07:41 -0300

    MEL4577

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-// Manipulação do formulário de login
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('loginForm');
-    
-    if (loginForm) {
-        loginForm.addEventListener('submit', function(e) {
-            e.preventDefault();
-            
-            const inputs = loginForm.querySelectorAll('input');
-            const usuario = inputs[0].value;
-            const senha = inputs[1].value;
-            
-            if (usuario && senha) {
-                console.log('Login realizado com sucesso');
-                // Aqui você adicionaria a lógica de autenticação real
-            } else {
-                alert('Preencha todos os campos');
-            }
-        });
-    }
-});
+function tocarMusica() {
+  let musica = document.getElementById("musica");
+
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+}
