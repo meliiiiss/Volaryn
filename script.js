6c4ede0cbfe2b421a67ee48a60b5841122f6d8ad
meliiiiss <melissabatgut@gmail.com>
2026-05-04 17:08:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function entrarSite() {
   document.getElementById("intro").style.display = "none";
   document.getElementById("site").style.display = "block";
 
-  // 🎵 música ao clicar em entrar
-  const musica = new Audio("assets/audio/theme.mp3");
-  musica.loop = true;
+  const musica = document.getElementById("musica");
   musica.play();
 }
