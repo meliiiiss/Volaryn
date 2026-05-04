1c4b1a04f0c85e48bd7a8c851595757ec6bae66e
meliiiiss <melissabatgut@gmail.com>
2026-05-04 16:30:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 /* 🌑 ENTRADA DO JOGO */
 function entrarSite() {
-  const intro = document.getElementById("intro");
-  const site = document.getElementById("site");
-
-  // esconde intro
-  intro.style.opacity = "0";
-  intro.style.transition = "0.8s";
-
-  setTimeout(() => {
-    intro.style.display = "none";
-    site.style.display = "block";
-  }, 800);
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("site").style.display = "block";
 }
