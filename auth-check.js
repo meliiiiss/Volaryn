db21ae99e48ce915200c5078ee1098e9060b082a
meliiiiss <melissabatgut@gmail.com>
2026-05-04 23:41:16 -0300

    Update auth-check.js

--- auth-check.js
+++ auth-check.js
@@ -1,18 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-import { getAuth, onAuthStateChanged } 
-from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyABZ0LjpdOqoN-fcSRkKUWOz8UngASJBC4",
-  authDomain: "volaryon-7af24.firebaseapp.com",
-  projectId: "volaryon-7af24"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "login.html";
-  }
-});
+// checagem simples de acesso
+if (localStorage.getItem("logado") === "true") {
+  console.log("Usuário já logado");
+} else {
+  console.log("Usuário não logado");
+}
