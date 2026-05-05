3dd42a7502edf68ff779dee4aaaf08cdc9291e62
meliiiiss <melissabatgut@gmail.com>
2026-05-05 10:27:00 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword 
-} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyABZ0LjpdOqoN-fcSRkKUWOz8UngASJBC4",
@@ -14,28 +11,22 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-/* 🧾 REGISTRO */
 window.registrar = function () {
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
 
   createUserWithEmailAndPassword(auth, email, senha)
-    .then(() => {
-      alert("Conta criada!");
-      window.location.href = "index.html";
-    })
-    .catch(e => alert("Erro: " + e.message));
+    .then(() => alert("Conta criada!"))
+    .catch(e => alert(e.message));
 }
 
-/* 🔑 LOGIN */
 window.login = function () {
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
 
   signInWithEmailAndPassword(auth, email, senha)
     .then(() => {
-      alert("Login feito!");
       window.location.href = "index.html";
     })
-    .catch(e => alert("Erro: " + e.message));
+    .catch(e => alert(e.message));
 }
