89d8af374d3355034ad062122da176b281db4a47
meliiiiss <melissabatgut@gmail.com>
2026-05-04 23:36:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-window.addEventListener("load", () => {
-  const musica = document.getElementById("musica");
-
-  musica.volume = 0.5;
-
-  const playPromise = musica.play();
-
-  if (playPromise !== undefined) {
-    playPromise.catch(() => {
-      console.log("Autoplay bloqueado pelo navegador");
-    });
-  }
-});
+function entrar() {
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}
